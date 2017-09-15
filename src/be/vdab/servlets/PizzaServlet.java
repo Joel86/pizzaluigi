1d3a6eb618bab89556376060d83458a380ff1bcb
Joel Vanherle <joel.vanherle@hotmail.com>
2017-09-15 10:46:18 +0200

    Connected pizzarepository class to jdbc database

--- src/be/vdab/servlets/PizzaServlet.java
+++ src/be/vdab/servlets/PizzaServlet.java
@@ -7,11 +7,13 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 import be.vdab.entities.Pizza;
 import be.vdab.repositories.PizzaRepository;
@@ -24,7 +26,7 @@ public class PizzaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     private static final String VIEW = "/WEB-INF/JSP/pizzas.jsp";
     private static final String PIZZAS_REQUESTS = "pizzasRequests"; 
-    private final PizzaRepository pizzaRepository = new PizzaRepository();
+    private final transient PizzaRepository pizzaRepository = new PizzaRepository();
     private String pizzaFotosPad;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		((AtomicInteger) this.getServletContext().getAttribute(PIZZAS_REQUESTS)).incrementAndGet();
@@ -41,5 +43,9 @@ public class PizzaServlet extends HttpServlet {
 	public void init() throws ServletException {
 		this.getServletContext().setAttribute(PIZZAS_REQUESTS, new AtomicInteger());
 		pizzaFotosPad = this.getServletContext().getRealPath("/pizzafotos");
+	}
+	@Resource(name = PizzaRepository.JNDI_NAME)
+	void setDataSource(DataSource dataSource) {
+		pizzaRepository.setDataSource(dataSource);
 	}
 }
